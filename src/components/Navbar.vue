3227ee8b95aa7e4507a37797aad23bd63804c13f
Caspec <quii2000@yahoo.dk>
2021-03-11 13:08:00 +0100

    -

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -34,7 +34,7 @@
       >
      </v-app-bar-nav-icon>
     </v-app-bar>
-    <v-navigation-drawer v-model="drawer" fixed right>
+    <v-navigation-drawer v-model="drawer" fixed temporary right>
       <v-list nav dense>
         <v-list-item-group
           v-model="group"
